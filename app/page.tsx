ca560ca96344712ac6e99ea7235b2a3a63036988
nicoszn <141965889+nicoszn@users.noreply.github.com>
2026-08-21 00:47:26 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import Landing from "@/components/Landing";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 justify-center bg-zinc-50 dark:bg-black">
-      <main className="w-full max-w-3xl px-6 py-14 sm:px-10">
+    <
         <Landing />
-      </main>
-    </div>
+    </>
   );
 }
