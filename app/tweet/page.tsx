2e2d903266dd2493a573d38887e6b984be134589
nicoszn <141965889+nicoszn@users.noreply.github.com>
2026-09-13 11:09:53 +0100

    Delete app/tweet directory

--- app/tweet/page.tsx
+++
@@ -1,9 +0,0 @@
-import MediaDownloader from '@/components/MediaDownloader';
-import ExtractorForm from '@/components/ExtractorForm';
-export default function Tweet() {
-  return (
-    <main className="min-h-screen bg-zinc-950 flex flex-col items-center justify-center p-4 selection:bg-blue-600 selection:text-white">
-      <ExtractorForm />
-    </main>
-  );
-}
