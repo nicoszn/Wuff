cbefae41a700be436d5add7e1ea383859c1c6bef
nicoszn <141965889+nicoszn@users.noreply.github.com>
2026-09-13 01:04:08 +0100

    Update page.tsx

--- app/tweet/page.tsx
+++ app/tweet/page.tsx
@@ -1,5 +1,10 @@
 import MediaDownloader from '@/components/MediaDownloader';
-
+import ExtractorForm from '@/components/ExtractorForm';
 export default function Tweet() {
-  return <MediaDownloader />;
+  // return <MediaDownloader />;
+  return (
+    <main className="min-h-screen bg-zinc-950 flex flex-col items-center justify-center p-4 selection:bg-blue-600 selection:text-white">
+      <ExtractorForm />
+    </main>
+  );
 }
