f5f59a6f200e34935917084169d72d114aa0ce06
nicoszn <141965889+nicoszn@users.noreply.github.com>
2026-09-13 01:06:27 +0100

    Uncomment MediaDownloader component in Tweet function

--- app/tweet/page.tsx
+++ app/tweet/page.tsx
@@ -1,7 +1,6 @@
 import MediaDownloader from '@/components/MediaDownloader';
 import ExtractorForm from '@/components/ExtractorForm';
 export default function Tweet() {
-  // return <MediaDownloader />;
   return (
     <main className="min-h-screen bg-zinc-950 flex flex-col items-center justify-center p-4 selection:bg-blue-600 selection:text-white">
       <ExtractorForm />
