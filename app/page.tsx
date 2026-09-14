a058ec414c06e1e25eed254010aa0ce1827b8f76
nicoszn <141965889+nicoszn@users.noreply.github.com>
2026-09-14 16:31:33 +0100

    Replace Landing component with VoiceLanding

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Landing from "@/components/Landing";
+import VoiceLanding from "@/components/VoiceLanding";
 
 export default function Home() {
   return (
     <>
-        <Landing />
+        <VoiceLanding />
     </>
   );
 }
